978c8ec44a136f6afd938e0408621e84675c18d6
Robin <lotosv2010@163.com>
2025-07-30 15:32:27 +0800

    feat(runtime-dom): 新建子包runtime-dom

---
+++ packages/runtime-dom/src/index.ts
@@ -0,0 +1,3 @@
+export function render(vnode, container) {}
+
+export * from '@g-vue-next/runtime-core'
--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -1,3 +1,4 @@
 export * from '@g-vue-next/shared'
 export * from '@g-vue-next/reactivity'
 export * from '@g-vue-next/runtime-core'
+export * from '@g-vue-next/runtime-dom'
